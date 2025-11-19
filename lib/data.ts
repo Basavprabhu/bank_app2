758e045fbf679995a8fbb85dd060c7262600a511
unknown <basavprabhu12345@gmail.com>
2025-11-19 22:37:35 +0530

    changed data to pavan

--- lib/data.ts
+++ lib/data.ts
@@ -19,63 +19,63 @@ export interface Account {
 }
 
 export const mockAccount: Account = {
-  customerName: "Ola Electric Technologies Pvt Ltd",
+  customerName: "Channappa Talawai",
   accountType: "Priority Account",
-  accountNumber: "1234567890123456",
-  balance: 2500000000.00,
+  accountNumber: "876790240091",
+  balance: 1071500000.00,
   ifscCode: "HDFC0001234",
-  branchName: "Bangalore - Electronic City",
-  customerId: "CUST001"
+  branchName: "Dharwad - NTTF Branch",
+  customerId: "CUST091675"
 };
 
 export const mockTransactions: Transaction[] = [
   {
     id: "1",
-    date: "2024-07-08",
-    description: "Online Transfer to Vendor Payment",
-    amount: -50000,
-    type: "debit",
-    balance: 2500000
+    date: "2025-11-17",
+    description: "TVS motor Software solutions India Pvt Ltd",
+    amount: 96500000.00,
+    type: "credit",
+    balance: 975000000.00
   },
   {
     id: "2",
-    date: "2024-07-05",
-    description: "Salary Credit - Monthly",
-    amount: 150000,
+    date: "2025-11-15",
+    description: "CreDha Shodha AutoService India Pvt Ltd",
+    amount: 185000000.00,
     type: "credit",
-    balance: 2550000
+    balance: 795000000.00
   },
   {
     id: "3",
-    date: "2024-07-02",
-    description: "Electricity Bill Payment",
-    amount: -15000,
-    type: "debit",
-    balance: 2400000
+    date: "2025-11-11",
+    description: "Cipla Phramaceuticals India Pvt Ltd",
+    amount: 102500000.00,
+    type: "credit",
+    balance: 610000000.00
   },
   {
     id: "4",
-    date: "2024-06-28",
-    description: "Business Income Credit",
-    amount: 300000,
+    date: "2025-11-07",
+    description: "Milton Industries India Pvt Ltd",
+    amount: 167500000.00,
     type: "credit",
-    balance: 2415000
+    balance: 507500000.00
   },
   {
     id: "5",
-    date: "2024-06-25",
-    description: "Office Rent Payment",
-    amount: -85000,
-    type: "debit",
-    balance: 2115000
+    date: "2025-10-23",
+    description: "SRV offshore Communications Pvt Ltd",
+    amount: 110000000.00,
+    type: "credit",
+    balance: 340000000.00
   },
   {
     id: "6",
-    date: "2024-06-20",
-    description: "Insurance Premium",
-    amount: -25000,
-    type: "debit",
-    balance: 2200000
+    date: "2025-09-29",
+    description: "Ola India Entreprises Pvt Ltd",
+    amount: 175000000.00,
+    type: "credit",
+    balance: 130000000.00
   }
 ];
 
