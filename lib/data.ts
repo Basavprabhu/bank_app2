33b72160c9c2c39cfd0bc138f33e3e9b418c9861
Basavprabhu <69919994+Basavprabhu@users.noreply.github.com>
2025-12-06 22:36:22 +0530

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -19,10 +19,10 @@ export interface Account {
 }
 
 export const mockAccount: Account = {
-  customerName: "Sri Raghavendra Electrical and Civil Contractor",
+  customerName: "Praveen Basalingol",
   accountType: "Priority Account",
   accountNumber: "017005500160",
-  balance: 427102000.00,
+  balance: 5202000.00,
   ifscCode: "HDFC0005162",
   branchName: "Dharwad - NTTF Branch",
   customerId: "566829483"
@@ -32,50 +32,50 @@ export const mockTransactions: Transaction[] = [
   {
     id: "1",
     date: "2025-11-17",
-    description: "TVS motor Software solutions India Pvt Ltd",
-    amount: 96524000.00,
+    description: "Samant Audi",
+    amount: 312400.00,
     type: "credit",
-    balance: 427102000.00
+    balance: 4971020.00
   },
   {
     id: "2",
     date: "2025-11-15",
-    description: "CreDha Shodha AutoService India Pvt Ltd",
-    amount: 89500000.00,
+    description: "Reliance Petroleum",
+    amount: 805000.00,
     type: "credit",
-    balance: 330578000.00
+    balance: 4100578.00
   },
   {
     id: "3",
     date: "2025-11-11",
-    description: "Cipla Phramaceuticals India Pvt Ltd",
-    amount: 10250000.00,
+    description: "Indian Oil Petroleum pvt ltd",
+    amount: 1650000.00,
     type: "credit",
-    balance: 241078000.00
+    balance: 2410780.00
   },
   {
     id: "4",
     date: "2025-11-07",
     description: "Milton Industries India Pvt Ltd",
-    amount: 45689000.00,
+    amount: 4568900.00,
     type: "credit",
-    balance: 195389000.00
+    balance: 1953890.00
   },
   {
     id: "5",
     date: "2025-10-23",
     description: "SRV offshore Communications Pvt Ltd",
-    amount: 54500000.00,
+    amount: 545000.00,
     type: "credit",
-    balance: 140889000.00
+    balance: 1408890.00
   },
   {
     id: "6",
     date: "2025-09-29",
     description: "Ola India Entreprises Pvt Ltd",
-    amount: 75465000.00,
+    amount: 754650.00,
     type: "credit",
-    balance: 65424000.00
+    balance: 6542400.00
   }
 ];
 
